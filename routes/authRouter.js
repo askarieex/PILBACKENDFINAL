0daa4631a79df6223111f0ebb207046d92834045
Mohammad Askarie <askarie@algodream.in>
2024-12-23 23:46:41 +0530

    fixing a register path

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -126,5 +126,6 @@ router.get("/admitCard", authMiddleware, AdmitCard);
 router.get("/syllabus", getAllSyllabus);
 router.get("/datesheet", getAllDatesheets); // Ensure getAllDatesheets is defined
 router.get("/messages", getAllMessages);
+router.get("/register", registerValidate(register));
 
 module.exports = router;
